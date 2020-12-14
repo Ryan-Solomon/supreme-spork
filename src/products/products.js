3b2668e8a92bf522ce619d60668ce0fe2de57c9d
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-14 14:10:43 -0600

    add generate and create product functions

--- src/products/products.js
+++ src/products/products.js
@@ -2,6 +2,7 @@
 const productGalleryParent = document.querySelector('.product-gallery');
 
 // Event Listeners
+generateProducts();
 
 // Functions
 
@@ -9,10 +10,41 @@ async function getProducts() {
   try {
     const res = await fetch('https://fakestoreapi.com/products');
     const data = await res.json();
-    console.log(data);
+    return data;
   } catch (e) {
     console.error(e.message);
+    return 'There was an error';
   }
 }
 
-getProducts();
+async function generateProducts() {
+  const products = await getProducts();
+  if (typeof products === 'string') return;
+
+  const productFragment = document.createDocumentFragment();
+  products.forEach((product) => {
+    let newProduct = createProduct(product);
+    productFragment.append(newProduct);
+  });
+  productGalleryParent.append(productFragment);
+}
+
+function createProduct(product) {
+  const { image, price, title, category } = product;
+  // Create Elements
+  const container = document.createElement('div');
+  container.classList.add(`product ${category}`);
+  const imageEle = document.createElement('img');
+  imageEle.src = image;
+  imageEle.alt = title;
+  const detailsDiv = document.createElement('div');
+  const titleElement = document.createElement('h4');
+  titleElement.textContent = title;
+  const priceElement = document.createElement('h4');
+  priceElement.textContent = price;
+  detailsDiv.append(titleElement, priceElement);
+
+  container.append(imageEle, detailsDiv);
+
+  return container;
+}
